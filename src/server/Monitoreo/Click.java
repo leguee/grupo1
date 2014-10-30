33a2bb37bf7dbb0cb039531a2bf7cdde64620932
legui <fnleguizamon@gmail.com>
2014-10-30 12:25:59 +0100

    Se implemento click y parser
    y el txt con los chorizos.

--- src/server/Monitoreo/Click.java
+++ src/server/Monitoreo/Click.java
@@ -1,5 +1,51 @@
 package server.Monitoreo;
 
 public class Click {
-
+	private String fecha;
+	private String hora;
+	private String browser;
+	private String so;
+	private String link;
+	private String ip;
+	
+	public String getFecha() {
+		return fecha;
+	}
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+	public String getHora() {
+		return hora;
+	}
+	public void setHora(String hora) {
+		this.hora = hora;
+	}
+	public String getBrowser() {
+		return browser;
+	}
+	public void setBrowser(String browser) {
+		this.browser = browser;
+	}
+	public String getSo() {
+		return so;
+	}
+	public void setSo(String so) {
+		this.so = so;
+	}
+	public String getLink() {
+		return link;
+	}
+	public void setLink(String link) {
+		this.link = link;
+	}
+	public String getIp() {
+		return ip;
+	}
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+	public Click() {
+	
+	}
+	
 }
--- src/server/Monitoreo/Parser.java
+++ src/server/Monitoreo/Parser.java
@@ -1,5 +1,39 @@
 package server.Monitoreo;
 
-public class Parser {
+import java.util.Vector;
 
+
+
+public class Parser {
+	
+	private Click click;
+	
+	public Parser() {
+		click = new Click();
+	}
+ 
+	public Click parsear( String p ) {
+		String palabra = p;
+		Vector<String> srtg = new Vector<String>();
+		String delims = "[$]+";
+		String[] tokens = palabra.split(delims);
+		
+		/////Muestra los tokens
+		for(String e: tokens){
+			e. trim();
+			srtg.add(e);
+			System.out.println(e+ ",");
+		}
+		/////
+		
+		click.setFecha(srtg.elementAt(0));
+		click.setHora(srtg.elementAt(1));
+		click.setBrowser(srtg.elementAt(2));
+		click.setSo(srtg.elementAt(3));
+		click.setLink(srtg.elementAt(4));
+		click.setIp(srtg.elementAt(5));
+		
+		return click;
+	}
+	
 }
